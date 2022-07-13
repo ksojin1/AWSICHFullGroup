1631ca279aafff7931c2018da745dabc5fb1f4dc
EZEN-134 <EZEN-134@DESKTOP-CB48U3F>
2022-07-13 15:51:45 +0900

    GitTest

---
+++ KSJ/src/KSJ.java
@@ -0,0 +1,13 @@
+
+public class KSJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NGH SDY GMJ");
+		System.out.println("KJS KTH PYM");
+		System.out.println("ABG LYB LJH");
+		System.out.println("JYS JHS HYJ");
+		System.out.println("GH DH EJ");
+	}
+
+}
